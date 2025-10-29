e108daf5f37122fcb58a96c47dcbbd9a978dfae2
jprice <jprice@izeni.com>
2025-10-29 03:08:04 -0600

    Add meta tags, favicon, and site manifest

--- utils/static/utils/markdown-editor.js
+++ utils/static/utils/markdown-editor.js
@@ -1,12 +1,12 @@
 (function () {
   var $ = django.jQuery;
   $(document).ready(function () {
-    $("textarea.markdown-editor").each(function (idx, el) {
+    $('textarea.markdown-editor').each(function (idx, el) {
       CodeMirror.fromTextArea(el, {
         lineNumbers: true,
         lineWrapping: true,
-        mode: "markdown",
-        theme: "monokai",
+        mode: 'markdown',
+        theme: 'monokai',
       });
     });
   });
